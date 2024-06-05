7949caca6fb5282722d2018924baac53603890fe
kowallus <kowallus@o2.pl>
2024-06-05 09:44:51 +0200

    new initial project state

--- src/main/java/edu/se/bowling/Game.java
+++
@@ -1,5 +0,0 @@
-package edu.se.bowling;
-
-public class Game {
-
-}
---
+++ src/test/java/edu/se/bowling/BowlingGameTest.java
@@ -0,0 +1,19 @@
+package edu.se.bowling;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class BowlingGameTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
