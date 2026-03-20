42dff4ab293700bf03347cbd7671dd8b25bec4b3
kowallus <kowallus@o2.pl>
2026-03-20 18:22:10 +0100

    game redesign - rolls array

--- src/main/java/edu/se/bowling/Game.java
+++ src/main/java/edu/se/bowling/Game.java
@@ -2,13 +2,18 @@ package edu.se.bowling;
 
 public class Game {
 
-	private int score = 0;
+	private int currentRoll = 0;
+	private int[] rolls = new int[21];
 
 	public void roll(int pins) {
-		score += pins;
+		rolls[currentRoll++] = pins;
 	}
 
 	public int score() {
+		int score = 0;
+		for (int rollIdx = 0; rollIdx < rolls.length; rollIdx++) {
+			score += rolls[rollIdx];
+		}
 		return score;
 	}
 
