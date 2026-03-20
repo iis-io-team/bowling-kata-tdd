8a06c2e3f2760857c533dceb049024a569329129
kowallus <kowallus@o2.pl>
2026-03-20 22:52:41 +0100

    refactor - extracting constants and methods

--- src/main/java/edu/se/bowling/Game.java
+++ src/main/java/edu/se/bowling/Game.java
@@ -2,8 +2,12 @@ package edu.se.bowling;
 
 public class Game {
 
+	private static final int NUMBER_OF_PINS = 10;
+	private static final int NUMBER_OF_FRAMES = 10;
+	private static final int MAX_NUMBER_OF_ROLLS = 21;
+	
 	private int currentRoll = 0;
-	private int[] rolls = new int[21];
+	private int[] rolls = new int[MAX_NUMBER_OF_ROLLS];
 
 	public void roll(int pins) {
 		rolls[currentRoll++] = pins;
@@ -12,13 +16,20 @@ public class Game {
 	public int score() {
 		int score = 0;
 		int rollIdx = 0;
-		for (int frame = 0; frame < 10; frame++) {
+		for (int frame = 0; frame < NUMBER_OF_FRAMES; frame++) {
 			score += rolls[rollIdx] + rolls[rollIdx + 1];
-			if (rolls[rollIdx] + rolls[rollIdx + 1] == 10) //spare
-				score += rolls[rollIdx + 2];
+			if (isSpare(rollIdx))
+				score += spareBonus(rollIdx);
 			rollIdx += 2;
 		}
 		return score;
 	}
 
+	private boolean isSpare(int rollIdx) {
+		return rolls[rollIdx] + rolls[rollIdx + 1] == NUMBER_OF_PINS;
+	}
+
+	private int spareBonus(int rollIdx) {
+		return rolls[rollIdx + 2];
+	}	
 }
