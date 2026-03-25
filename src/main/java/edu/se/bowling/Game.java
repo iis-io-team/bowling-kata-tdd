8bebd815d99f061fc811d8cf2d48e191c00d2d02
kowallus <kowallus@o2.pl>
2026-03-25 11:15:49 +0100

    redesigning game - loop over frame

--- src/main/java/edu/se/bowling/Game.java
+++ src/main/java/edu/se/bowling/Game.java
@@ -2,13 +2,20 @@ package edu.se.bowling;
 
 public class Game {
 
-	private int score = 0;
+	private int[] rolls = new int[21];
+	private int currentRoll = 0;
 
 	public void roll(int pins) {
-		score += pins;
+		rolls[currentRoll++] = pins;
 	}
 
 	public int score() {
+		int score = 0;
+		int rollIdx = 0;
+		for (int frameIdx = 0; frameIdx < 10; frameIdx++) {
+			score += rolls[rollIdx] + rolls[rollIdx + 1];
+			rollIdx += 2;
+		}
 		return score;
 	}
 
