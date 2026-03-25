4218e83c2717b489877683e18c2eb828d2873009
kowallus <kowallus@o2.pl>
2026-03-25 11:03:05 +0100

    all ones game test

--- src/test/java/edu/se/bowling/BowlingGameTest.java
+++ src/test/java/edu/se/bowling/BowlingGameTest.java
@@ -20,4 +20,13 @@ public class BowlingGameTest {
 		assertEquals(0, g.score());
 	}
 
+	@Test
+	public void allOneGameScoresTwenty() {
+		Game g = new Game();
+		for (int i = 0; i < 20; i++) {
+			g.roll(1);
+		}
+		assertEquals(20, g.score());
+	}
+
 }
