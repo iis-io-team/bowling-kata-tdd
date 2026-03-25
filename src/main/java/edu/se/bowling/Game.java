72e2000048a658a8e24e93b1ea3c75ad8ea713f7
kowallus <kowallus@o2.pl>
2026-03-25 11:30:40 +0100

    refactor and cleanup - extracted spare methods and constants

--- src/main/java/edu/se/bowling/Game.java
+++ src/main/java/edu/se/bowling/Game.java
@@ -2,7 +2,10 @@ package edu.se.bowling;
 
 public class Game {
 
-	private int[] rolls = new int[21];
+	private static final int NUMBER_OF_FRAMES = 10;
+	private static final int MAX_NUMBER_OF_ROLLS = 21;
+	private static final int NUMBER_OF_PINS = 10;
+	private int[] rolls = new int[MAX_NUMBER_OF_ROLLS];
 	private int currentRoll = 0;
 
 	public void roll(int pins) {
@@ -12,13 +15,22 @@ public class Game {
 	public int score() {
 		int score = 0;
 		int rollIdx = 0;
-		for (int frameIdx = 0; frameIdx < 10; frameIdx++) {
-			if (rolls[rollIdx] + rolls[rollIdx + 1] == 10) // spare
-				score += rolls[rollIdx + 2]; //spare bonus
+		for (int frameIdx = 0; frameIdx < NUMBER_OF_FRAMES; frameIdx++) {
+			if (isSpare(rollIdx)) {
+				score += spareBonus(rollIdx);
+			}
 			score += rolls[rollIdx] + rolls[rollIdx + 1];
 			rollIdx += 2;
 		}
 		return score;
+	}
+
+	private int spareBonus(int frameRollIdx) {
+		return rolls[frameRollIdx + 2];
+	}
+
+	private boolean isSpare(int frameRollIdx) {
+		return rolls[frameRollIdx] + rolls[frameRollIdx + 1] == NUMBER_OF_PINS;
 	}
 
 }
