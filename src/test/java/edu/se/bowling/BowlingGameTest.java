8e8ba5a1e9a2e87baa5a1bd2604f0dfa03f48aca
kowallus <kowallus@o2.pl>
2026-03-25 10:54:03 +0100

    first test - gutter game

--- src/test/java/edu/se/bowling/BowlingGameTest.java
+++ src/test/java/edu/se/bowling/BowlingGameTest.java
@@ -3,7 +3,7 @@ package edu.se.bowling;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.fail;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class BowlingGameTest {
 
@@ -12,8 +12,12 @@ public class BowlingGameTest {
 	}
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void gutterGameScoresZero() {
+		Game g = new Game();
+		for (int i = 0; i < 20; i++) {
+			g.roll(0);
+		}
+		assertEquals(0, g.score());
 	}
 
 }
