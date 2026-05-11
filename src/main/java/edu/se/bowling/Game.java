0a984dd3a3487981d0e9ac177de6342d7948991d
kowallus <kowallus@o2.pl>
2026-05-11 09:23:16 +0200

    spare bonus logic

--- src/main/java/edu/se/bowling/Game.java
+++ src/main/java/edu/se/bowling/Game.java
@@ -12,7 +12,10 @@ public class Game {
 	public int score() {
 		int score = 0;
 		int i = 0;
-		for (int frame = 0; frame < 10; frame++) {			
+		for (int frame = 0; frame < 10; frame++) {
+			if (rolls[i] + rolls[i + 1] == 10) { // spare
+				score += rolls[i + 2];
+			}
 			score += rolls[i] + rolls[i + 1];
 			i += 2;
 		}
